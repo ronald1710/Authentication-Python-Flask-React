cf2ba89e1542af665e4f711653d745a56761c89f
ronald1710 <ronald.azofeifa@yahoo.com>
2022-10-16 04:13:11 +0000

    2

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -7,7 +7,28 @@ export const Login = () => {
   const { store, actions } = useContext(Context);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const handleClick = () => {};
+  const handleClick = () => {
+    const opts = {
+      method: "POST",
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    };
+
+    fetch(
+      "https://3001-4geeksacade-reactflaskh-lkofv1dbzx4.ws-us71.gitpod.io/api/token",
+      opts
+    )
+      .then((resp) => {
+        if (resp.status === 200) return resp.json();
+        else alert("There has been some error");
+      })
+      .then()
+      .catch((error) => {
+        console.error("There was an error", error);
+      });
+  };
 
   return (
     <div className="text-center mt-5">
